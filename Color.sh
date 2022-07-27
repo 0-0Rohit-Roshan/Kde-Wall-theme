e071eeca1e88a877682f9bd4594ea64f6642bf92
0-0Rohit-Roshan <rohitroshanjena2017@gmail.com>
2022-07-27 12:33:37 +0530

    Feat : rename before selecting image

--- Color.sh
+++ Color.sh
@@ -2,13 +2,14 @@
 
 cd Pictures/
 
-img=$(sxiv *.jpg -o)
+source "RenameToIntiger.sh"
+img=$(sxiv *.jpg -o -q)
 #echo $img
-wal -i=$img -b=#000000 -n
+wal -i=$img -b=#000000 -n -q
 
 #imgpath=/home/miku/Pictures/$img
 
-cat ../Wallpaper.sh |tr 12345 $img >../wallpaper.sh
+cat ../Wallpaper.sh |tr 12345678 $img >../wallpaper.sh
 #echo "Your imgpath = $imgpath"
 #cp -f ../Wallpaper.sh ../wallpaper.sh
 #cat ../wallpaper.sh
@@ -18,3 +19,9 @@ source "../wallpaper.sh"
 notify-send "wallpaper set as $img and appropriate terminal color applied"
 
 rm ../wallpaper.sh
+
+#Todo
+#Move the wallpapers to the windows file system.
+#Add more dynamicity in script to rename all jpg file in the directory to intiger to avoid incompatibility at tr command (check if a single renaming script can be run in multiple diectory by the main script.)
+#Try to do it for Konsole.
+#Acces it from any directory.
